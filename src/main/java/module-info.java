61ad989b16d6f29e5c64d44d298751dbf1cdd436
溝口顕一 <hachirouuemon.mizoguchi@gmail.com>
2020-02-01 16:23:01 +0900

    公開パッケージを追加した

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module venus.utils {
+    exports org.venuspj.util.annotations;
     exports org.venuspj.util.base;
     exports org.venuspj.util.beans;
     exports org.venuspj.util.beans.factory;
@@ -6,20 +7,22 @@ module venus.utils {
     exports org.venuspj.util.collect;
     exports org.venuspj.util.convert;
     exports org.venuspj.util.dateProvider;
+    exports org.venuspj.util.exception;
     exports org.venuspj.util.lang;
     exports org.venuspj.util.message;
     exports org.venuspj.util.objects2;
     exports org.venuspj.util.primitives;
+    exports org.venuspj.util.reflection;
     exports org.venuspj.util.strings2;
     exports org.venuspj.util.uuidProvider;
     exports org.venuspj.util.validate;
-    exports org.venuspj.util.exception;
     requires java.sql;
     requires java.desktop;
 //    requires kotlin.stdlib;
     opens org.venuspj.util.beans;
     opens org.venuspj.util.beans.impl;
     opens org.venuspj.util.beans.factory;
+    opens org.venuspj.util.reflection;
     opens org.venuspj.util.lang;
 
 
