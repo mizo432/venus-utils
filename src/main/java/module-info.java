bcb186c533fed00bdc1d933fae4a0ef2616d5af6
mizoguchi kenichi <hachirouuemon.mizoguchi@gmail.com>
2024-04-29 04:14:16 +0900

    java21対応

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,21 @@ module venus.utils.main {
   requires java.logging;
   requires java.desktop;
   requires java.sql;
+  exports org.venuspj.util.annotations;
+  exports org.venuspj.util.base;
+  exports org.venuspj.util.beans;
+  exports org.venuspj.util.convert;
+  exports org.venuspj.util.collect;
+  exports org.venuspj.util.builder;
+  exports org.venuspj.util.exception;
+  exports org.venuspj.util.dateProvider;
+  exports org.venuspj.util.primitives;
+  exports org.venuspj.util.io;
+  exports org.venuspj.util.lang;
+  exports org.venuspj.util.misc;
+  exports org.venuspj.util.math;
+  exports org.venuspj.util.objects2;
+  exports org.venuspj.util.uuidProvider;
+  exports org.venuspj.util.text;
+  exports org.venuspj.util.beans.factory;
 }
